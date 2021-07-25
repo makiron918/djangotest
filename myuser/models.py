f620819de1ed49d6957c618f622ca2d3bf88ccef
makiron918 <gonzaress918@gmail.com>
2021-07-25 09:29:18 +0900

    Update models.py

--- myuser/models.py
+++ myuser/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MyUser(models.Model):
+    username = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField()
